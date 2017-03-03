106a5b52cdfd576a32c6aaf9331248e417e9edaf
Marco Fincato <marcofincato777@gmail.com>
2017-03-03 12:55:29 +0100

    fixed conflicts

--- MazeBot/Moviment/Moviment.cpp
+++ MazeBot/Moviment/Moviment.cpp
@@ -1,13 +1,5 @@
 #include "Moviment.h"
 
-// defining constant pin for motors
-#define R_EN 4
-#define R_IN1 3
-#define R_IN2 2
-#define L_EN 7
-#define L_IN1 5
-#define L_IN2 6
-
 Moviment::Moviment (int speed, int rightK, int leftK) {
   this->speed = speed;
   kR = rightK;
