7b74e545b9ffc6063f44059cdcc3183da271df24
UNicoTF <nico.taglia1572000@gmail.com>
2017-03-18 22:55:17 +0100

    indent

--- maze-bot.ino
+++ maze-bot.ino
@@ -10,32 +10,39 @@ Motion mov;
 Matrix mat; // Matrice che rappresenta il maze
 ColorIR color; // Sensore di colore
 // Sensori di distanza
-DistanceIR infrared [8] = {DistanceIR(A0, 25, 93), DistanceIR(A1, 25, 93), DistanceIR(A2, 25, 93), DistanceIR(A3, 25, 93), DistanceIR(A4, 25, 93), DistanceIR(A5, 25, 93), DistanceIR(A6, 25, 93), DistanceIR(A7, 25, 93)};
+DistanceIR infrared[8] = {DistanceIR(A0, 25, 93), DistanceIR(A1, 25, 93), DistanceIR(A2, 25, 93),
+                          DistanceIR(A3, 25, 93), DistanceIR(A4, 25, 93), DistanceIR(A5, 25, 93),
+                          DistanceIR(A6, 25, 93), DistanceIR(A7, 25, 93)};
 // usltrasonic[n] : n = : 0 = destra, 1 = dietro, 2 = sinistra, 3 = avanti
-DistanceUS ultrasonic [4] = {DistanceUS(44, 45, 5, 93), DistanceUS(46, 47, 5, 93), DistanceUS(48, 49, 5, 93), DistanceUS(50, 51, 5, 93)};
+DistanceUS ultrasonic[4] = {DistanceUS(44, 45, 5, 93), DistanceUS(46, 47, 5, 93), DistanceUS(48, 49, 5, 93),
+                            DistanceUS(50, 51, 5, 93)};
 
-Temperature temps [2] = {Temperature(0x5A), Temperature(0x5C)}; // Sensori temperatura
+Temperature temps[2] = {Temperature(0x5A), Temperature(0x5C)}; // Sensori temperatura
 
-void drive(){  /// Funzione che guida tutto
-  float dist = ultrasonic[3].read() - 30;
-  mov.go();
-  bool black = false;
-  while (ultrasonic[3].read() > dist){
-    /* Controllo del nero
-    se è nero torna indietro e gira
-    se è nero black = true;
-    */
-  }
-  if (!black) {
-    mov.stop();
-  }
+void drive() {  /// Funzione che guida tutto
+    //mat.check();
+    float dist = ultrasonic[3].read() - 30;
+    mov.go();
+    bool black = false;
+    while (ultrasonic[3].read() > dist) {
+        if (color.read() == 2) {
+            black = true;
+        }
+        /* Controllo del nero
+        se è nero torna indietro e gira
+        se è nero black = true;
+        */
+    }
+    if (!black) {
+        mov.stop();
+    }
 }
 
-void setup(){
-  Serial.begin(9600);
-  Serial.println("Avvio!");
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Avvio!");
 }
 
-void loop(){
-  drive();
+void loop() {
+    drive();
 }
