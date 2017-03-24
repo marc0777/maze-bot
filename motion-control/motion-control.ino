dbb43024e78e74d9a35c31068791fbfac9ac52b1
NekoAndLoli <vx1245399319@gmail.com>
2017-03-24 13:31:06 +0100

    fatta la funzione da chiamare mentre il robot gira
    regola i coefficienti in base a quanti gradi deve ancora girare. Calcola partendo dai 40 perchè con una velocità inferiore a 140 il robot non girava, il rapportoVR è ottenuto dal rapporto fra la differenza di velocità massima (ossia 60, cioè il doppio della velocità solita) e la rotazione massima (90 gradi)
    
    formula : v=140+(rM*deltaV/deltaR)
    v: velocità ottenuta
    rM: rotazione rimanente da fare
    deltaV: differenza di velocità massima
    deltaR: differenza di rotazione massima

--- motion-control/motion-control.ino
+++ motion-control/motion-control.ino
@@ -1,12 +1,13 @@
 #include <Arduino.h>
-
+#include "MadgwickAHRS.h"
 #include <Wire.h>
 #include <Moviment.h>
 
 #define ADDRESS 7
 
+float rapportoVR=60/90;//deltaV/deltaR
 byte state = 0;
-
+Madgwick filter;
 Moviment mov(100,0,0);
 
 void receiveEvent(int howMany) {
@@ -16,6 +17,24 @@ void receiveEvent(int howMany) {
 
 void requestEvent() {
   Wire.write(mov.get());
+}
+
+void rotationSpeed(bool direction , float endRotation){
+  if(direction)mov.setK(40+((endRotation-filter.getYaw())*rapportoVR),40+((endRotation-filter.getYaw())*rapportoVR));
+  else mov.setK(40+((filter.getYaw()-endRotation)*rapportoVR),40+((filter.getYaw()-endRotation)*rapportoVR));
+}// negare la condizione se il filtro funziona in modo diverso
+
+float endAngle(float n, bool s){
+  if(s){
+    n+=90;
+    if(n>360)return n-360;
+    else return n;
+  }
+  else {
+    n-=90;
+    if(n<0)return n+360;
+    else return n;
+  }
 }
 
 void setup() {
