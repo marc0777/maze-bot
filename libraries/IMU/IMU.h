ec7c4f4bc6880eb8d4cd9143c8cfccf2181751a8
Marco Fincato <marcofincato777@gmail.com>
2017-03-27 23:28:46 +0200

    splitted up from main .ino the IMU control

---
+++ libraries/IMU/IMU.h
@@ -0,0 +1,28 @@
+#ifndef IMU_h
+#define IMU_h
+
+#include <MadgwickAHRS.h>
+#include <L3G4200D.h>
+#include <ADXL345.h>
+
+#define REFRESH 50
+
+class IMU {
+  public:
+    IMU();
+    void begin();
+    void calibrate();
+    void start();
+    float yaw();
+    float pitch();
+    float roll();
+  private:
+    void update();
+    L3G4200D gyroscope;
+    ADXL345 acc;
+    Madgwick filter;
+    unsigned long microsPerReading;
+    unsigned long microsPrevious;
+};
+
+#endif
