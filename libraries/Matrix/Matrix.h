95a6fa2b396938f26f320724600cdcfd132fa449
Nicolò Tagliaferro <nico.taglia1572000@gmail.com>
2017-04-11 22:33:19 +0200

    Bug fix

--- libraries/Matrix/Matrix.h
+++ libraries/Matrix/Matrix.h
@@ -6,52 +6,55 @@
 
 #define DELTATEMP 10
 #define DISTWALL 10
+#define NCELLS 31
 
 class Matrix {
 public:
-  Matrix();
+    Matrix();
 
-  void check(float tempDx, float tempSx, float distDx, float distSx, float inclination, byte color);
+    void check(float tempDx, float tempSx, float distDx, float distSx, float inclination, byte color);
 
-  void changeFloor();
+    void changeFloor();
 
-  bool frontCheck();
+    bool frontCheck();
 
-  bool frontBlack();
+    bool frontBlack();
 
-  void reset();
+    void reset();
 
-  void go();
+    void go();
 
-  void back();
+    void back();
 
-  byte getDir(float dx, float front, float sx);  /// Ritorna 1 per destra, 2 per avanti, 3 per sinistra, 4 dietro
+    byte getDir(float dx, float front, float sx);  /// Ritorna 1 per destra, 2 per avanti, 3 per sinistra, 4 dietro
 
-  void rotate(bool direction);
+    void rotate(bool direction);
 
-  bool isHot();
+    bool isHot();
 
-  bool isVisited();
+    bool isVisited();
+
+    bool keep;
 
 private:
-  // typedef struct {
-  //   bool visited;
-  //   bool hot;
-  //   bool black;
-  //   byte direction;
-  // } cell;
-  Cell maze[2][19][19];
-  bool floor;
-  // Posizione attuale
-  byte r[2];
-  byte c[2];
-  byte dir; // 0 = nord, 1 = est, 2 = sud, 3 = ovest
-  // Coordinate dell'ultimo checkpoint
-  byte checkr;
-  byte checkc;
-  byte checkfl;
-  bool rise;
-  int cont;
+    // typedef struct {
+    //   bool visited;
+    //   bool hot;
+    //   bool black;
+    //   byte direction;
+    // } cell;
+    Cell maze[2][19][19];
+    bool floor;
+    // Posizione attuale
+    byte r[2];
+    byte c[2];
+    byte dir; // 0 = nord, 1 = est, 2 = sud, 3 = ovest
+    // Coordinate dell'ultimo checkpoint
+    byte checkr;
+    byte checkc;
+    byte checkfl;
+    bool rise;
+    int cont;
 };
 
 #endif //MATRIX_H
