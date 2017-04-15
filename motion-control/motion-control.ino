657894c1173046e068520171e312e459840c1f46
Marco Fincato <marcofincato777@gmail.com>
2017-04-15 16:25:20 +0200

    Added turn function, fixed bugs and reduced memory use

--- motion-control/motion-control.ino
+++ motion-control/motion-control.ino
@@ -8,42 +8,30 @@
 #define OUT_MIN 100
 #define OUT_MAX 255
 
-double Setpoint=inputPID=outputPID=0;//copiato dall'esempio della libreria del PID
-double Kp=2, Ki=5, Kd=1;//valori di default del PID da mettere a posto
+#define Kp 2
+#define Ki 5
+#define Kd 1
+
+double Setpoint, inputPID, outputPID; //copiato dall'esempio della libreria del PID
 PID pid(&inputPID, &outputPID, &Setpoint, Kp, Ki, Kd, DIRECT);  //inizializzazione dell'oggetto PID copiato senza capire cos'è il DIRECT
-                                                            //che non è stato trovato da nessuna parte, ma stranamente compila
-                                                            //l'input e l'output sono le variabili dove bisognerà assegnare il valore
-                                                            //letto il Setpoint è la variabile di riferimento, le tre costanti sono i
-                                                            //parametri da mettere apposto.
+//che non è stato trovato da nessuna parte, ma stranamente compila
+//l'input e l'output sono le variabili dove bisognerà assegnare il valore
+//letto il Setpoint è la variabile di riferimento, le tre costanti sono i
+//parametri da mettere apposto.
 
 
 float rapportoVR = 60 / 90; //deltaV/deltaR
 float direzione = 0;
-byte state = 0;
+byte state = -1;
+byte prevState = -1;
 
-Moviment mov(100, 0, 0);
+Moviment mov(255, 0, 0);
 IMU orientation;
 
 void receiveEvent(int howMany) {
   state = Wire.read();
-  switch (state) {
-    case 0:
-      mov.stop();
-      break;
-    case 1:
-      goStraight(false);
-      break;
-    case 2:
-      mov.rotate(false);
-      break;
-    case 3:
-      goStraight(true);
-      break;
-    case 4:
-      mov.rotate(true);
-      break;
-  }
 }
+
 
 void requestEvent() {
   Wire.write(state);
@@ -58,7 +46,7 @@ void rotationSpeed(bool direction , float endRotation) {
 void goStraight(bool invert) {
   orientation.start();
   mov.go(invert);
-  while(state==1) {
+  while (state == 1 || state == 3) {
     direzione = orientation.yaw();
     if (direzione < 0) mov.setK(-direzione * 2, 0);
     else if (direzione > 0) mov.setK(0, direzione * 2);
@@ -68,36 +56,71 @@ void goStraight(bool invert) {
 void goStraightPID(bool invert) {
   orientation.start();
   mov.go(invert);
-  while(state==1) {
+  while (state == 1 || state == 3) {
     direzione = orientation.yaw();
-    inputPID=direzione;
-    pid.Conpute();
+    inputPID = direzione;
+    pid.Compute();
     if (direzione < 0) mov.setK(outputPID, -outputPID);
-    else if (direzione > 0) mov.setK(-outputPID,pitoutPID);
+    else if (direzione > 0) mov.setK(-outputPID, outputPID);
   }
 }
 
-float endAngle(float n, bool s) {
-  if (s) {
-    n += 90;
-    return (n > 360)?n-360:n;
+void turn(bool invert) {
+  orientation.start();
+  mov.rotate(invert);
+  float end = endAngle(orientation.yaw(), invert);
+  if (invert) while(orientation.yaw()<end);
+  else while(orientation.yaw()>end);
+  mov.stop();
+}
+
+float endAngle(float angle, bool invert) {
+  if (invert) {
+    angle += 50;
+    return (angle > 360) ? angle - 360 : angle;
   }
   else {
-    n -= 90;
-    return (n < 0)?n+360:n;
+    angle -= 50;
+    return (angle < 0) ? angle + 360 : angle;
   }
 }
 
 void setup() {
   Wire.begin(ADDRESS);
   Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
+  //Wire.onRequest(requestEvent);
   orientation.begin();
   delay(100);
   orientation.calibrate();
   delay(100);
-  pid.SetOutputLimits(OUT_MIN,OUT_MAX);
+  pid.SetOutputLimits(OUT_MIN, OUT_MAX);
 }
 
 void loop() {
+  if (state!=prevState) {
+    switch (state) {
+    case 0:
+      mov.stop();
+      break;
+    case 1:
+      goStraight(false);
+      break;
+    case 2:
+      turn(false);
+      break;
+    case 3:
+      goStraight(true);
+      break;
+    case 4:
+      turn(true);
+      break;
+    case 5:
+      mov.rotate(false);
+      break;
+    case 6:
+      mov.rotate(true);
+      break;
+    }
+    prevState=state;
+  }
 }
