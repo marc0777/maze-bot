c06f9a00f197c571d6d99ce00e588ec17576f964
Marco Fincato <marcofincato777@gmail.com>
2017-04-18 11:36:45 +0200

    Added tools and useful code

---
+++ libraries/IMU/IMU.ino
@@ -0,0 +1,21 @@
+#include <IMU.h>
+
+IMU orientation;
+
+void setup() {  
+  Serial.begin(9600);
+  orientation.begin();
+  delay(100);
+  orientation.calibrate();
+  delay(100);
+  orientation.start();
+}
+
+void loop() {
+	Serial.print("Yaw: ");
+	Serial.print(orientation.yaw());
+	Serial.print("\tPitch: ");
+	Serial.print(orientation.pitch());
+	Serial.print("\tRoll: ");
+	Serial.println(orientation.roll());
+}
---
+++ tools/Controller/Controller.ino
@@ -0,0 +1,26 @@
+#include "Motion.h"
+
+Motion mot;
+byte data;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Pronto ad inviare");
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+      data=Serial.read()-'0';
+      Serial.print("Invio dati: ");
+      Serial.println(data);
+      mot.set(data);
+    }
+}
+
+
+
+
+
+
+
+
